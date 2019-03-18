4bf4dd600288584424ceb633feeb57a549dd06e3
Sez Flynn <SezF@provoke.co.nz>
2019-03-19 11:53:37 +1300

    yoeman scaffolding of documentcard webpart and project

---
+++ FabricReact/src/webparts/documentCardComponent/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDocumentCardComponentWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DocumentCardComponentWebPartStrings' {
+  const strings: IDocumentCardComponentWebPartStrings;
+  export = strings;
+}
